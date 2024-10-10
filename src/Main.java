d5ac174a216a38aa4cd281afbf87877adb2c2a98
lycmoons <2915017352@qq.com>
2024-10-10 14:57:56 +0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
         // hhhh
+        yduahidohihf
+                bfdiab
+                faofoqfj
+                        afihuefhuh
+                        fasf
     }
 }
